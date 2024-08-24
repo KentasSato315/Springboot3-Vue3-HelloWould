4a2a7c06a6152677391b58dd760fe202a0596830
kenta.sato <kenta.sato@wealthnavi.com>
2024-08-24 10:49:38 +0900

    create Springboot3 project

---
+++ src/main/java/org/example/springboot3vue3hellowould/Springboot3Vue3HelloWouldApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboot3vue3hellowould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3Vue3HelloWouldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3Vue3HelloWouldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springboot3vue3hellowould/Springboot3Vue3HelloWouldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot3vue3hellowould;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3Vue3HelloWouldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
